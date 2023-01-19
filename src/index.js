bc0d51acbc360033c3f3897179efa6f5d999eccc
Turchynyk Oleksii <105796938+TurchynykOleksii@users.noreply.github.com>
2023-01-20 01:57:01 +0200

    fix reload

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react_admin_dashboard">
       <App />
     </BrowserRouter>
   </React.StrictMode>
